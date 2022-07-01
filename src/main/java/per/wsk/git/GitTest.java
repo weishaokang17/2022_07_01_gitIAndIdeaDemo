ac38476fa3e4d53d5fd3c79cc1c64c4bfaff487d
weishaokang <weishaokang17@163.com>
2022-07-01 21:56:59 +0800

    master test的提交

--- src/main/java/per/wsk/git/GitTest.java
+++ src/main/java/per/wsk/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("第四次修改添加的内容");
         System.out.println("--------------------------");
+        System.out.println("master test");
     }
 }
