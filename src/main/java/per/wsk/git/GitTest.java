4a83be0ee09d22101d30bc7d54bfda5a17bc14bc
weishaokang <weishaokang17@163.com>
2022-07-01 19:38:45 +0800

    第四次提交

--- src/main/java/per/wsk/git/GitTest.java
+++ src/main/java/per/wsk/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("第四次修改添加的内容");
         System.out.println("--------------------------");
     }
 }
